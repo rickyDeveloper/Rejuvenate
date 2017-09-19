306f0224729f93b88f9d6b73c22580b98ccd072d
“Vikas <naiyarv@gmail.com>
2017-09-20 04:11:02 +0530

    Update the code

--- src/app.js
+++ src/app.js
@@ -8,7 +8,40 @@ var myApp = angular.module('myApp',['ngRoute','ngAnimate', 'ui.router']);
 /**
  * Created by vikasnaiyar on 10/09/16.
  */
-var myApp = angular.module('myApp',['ngRoute','ngAnimate', 'ui.router', 'ngStorage']);
+var myApp = angular.module('myApp',['ngRoute','ngAnimate', 'ui.router', 'ngStorage','lr.upload']);
+
+
+
+myApp.directive('fileModel', ['$parse', function ($parse) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
+
+            element.bind('change', function(){
+                scope.$apply(function(){
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
+}]);
+
+myApp.service('fileUpload', ['$http', function ($http) {
+    this.uploadFileToUrl = function(file, uploadUrl) {
+        var fd = new FormData();
+        fd.append('file', file);
+        $http.post(uploadUrl, fd, {
+                transformRequest: angular.identity,
+                headers: {'Content-Type': undefined}
+            })
+            .success(function () {
+            })
+            .error(function () {
+            });
+    }
+}]);
 
 /*myApp.config(['$routeProvider','$stateProvider', '$urlRouterProvider',
     function($routeProvider, $stateProvider, $urlRouterProvider) {
--- src/component/home/HomeController.js
+++ src/component/home/HomeController.js
@@ -8,49 +8,9 @@
 
 // our controller for the form
 // =============================================================================
-myApp.controller('HomeController', function($scope, $http, $sce, $localStorage, SharedDataService) {
-
-    /*
-    // we will store all of our form data in this object
-    $scope.formData = {};
-
-    // function to process the form
-    $scope.processForm = function() {
-        alert('awesome!');
-    };*/
-
+myApp.controller('HomeController', function($scope, $http, $sce, $localStorage,fileUpload) {
 
     $scope.init = function () {
-      $http({
-            method: 'GET',
-            url: 'http://54.210.170.189:9000/nodes/children/1'
-        }).then(function successCallback(response) {
-            // this callback will be called asynchronously
-            // when the response is available
-            $scope.moods = response.data
-        }, function errorCallback(response) {
-            // called asynchronously if an error occurs
-            // or server returns response with an error status.
-        });
-
-
-        /*$scope.moods = [
-
-            {
-                "id":1,
-            "feeling" : 'DEPRESSED',
-        "isLeaf":false
-
-            },
-
-            {
-                "id":2,
-                "feeling" : 'STRESSED',
-                "isLeaf":false
-
-            }
-
-        ]*/
 
     }
 
@@ -66,6 +26,10 @@ myApp.controller('HomeController', function($scope, $http, $sce, $localStorage, 
     vm.user.reasonSelection = ''
 
 
+    vm.fileTypes=["Resume", "Job Description"];
+
+    $scope.selectedFileType = "Resume";
+
     vm.user.whyOptions = []
     //vm.user.reasonSelections = []
 
@@ -76,76 +40,63 @@ myApp.controller('HomeController', function($scope, $http, $sce, $localStorage, 
     // Reason selection
     $scope.reasonSelections = []
 
+    $localStorage.fileType = "Resume"
+
+    vm.successMsg="";
     //Model
     vm.currentStep = 1;
     vm.steps = [
         {
             step: 1,
-            name: "Your Mood",
+            name: "Upload Document",
             template: "src/component/home/step1.html"
         },
         {
             step: 2,
-            name: "Reason",
+            name: "View Tags",
             template: "src/component/home/step2.html"
         },
         {
             step: 3,
-            name: "Due to",
+            name: "Matching",
             template: "src/component/home/step3.html"
         }
     ];
 
     vm.user = {};
 
+   // vm.documentTags = {};
+
+    vm.resumeId = "59c17b1c77c8e33c1e766dc7";
+    vm.jdId = "59c00c6930528df66672e73f";
+
+
     //Functions
     vm.gotoStep = function(newStep) {
         if(vm.currentStep == 1) {
-            vm.user.whyOptions = []
+            //vm.user.whyOptions = []
             $scope.selection = []
+
+            var link = ""
+            if($localStorage.fileType == "Resume") {
+                link = 'http://localhost:8080/files/resume/59c17b1c77c8e33c1e766dc7'
+            } else {
+                link = 'http://localhost:8080/files/jd/59c00c6930528df66672e73f'
+            }
 
             $http({
                 method: 'GET',
                 //url: '/nodes/children/'+vm.user.mood
-                url: 'http://54.210.170.189:9000/nodes/children/'+vm.user.mood
+                url:link
             }).then(function successCallback(response) {
                 // this callback will be called asynchronously
                 // when the response is available
-                vm.user.whyOptions = response.data
+                var str = JSON.stringify(response.data, undefined, 4);
+                vm.documentTags = str;
             }, function errorCallback(response) {
                 // called asynchronously if an error occurs
                 // or server returns response with an error status.
             });
-
-
-            /*$scope.stressed = ['Angry', 'Sad'],
-                $scope.depressed = ['Relation Ship', 'No Encouragement', 'No Reason'],
-                $scope.other = ['No aspiration', 'Low on confidence']
-
-            */
-
-            /*vm.user.whyOptions = [
-                {
-                    "id":3,
-                    "feeling" : 'Relation Ship',
-                    "isLeaf":false
-
-                },
-
-                {
-                    "id":4,
-                    "feeling" : 'No Encouragement',
-                    "isLeaf":false
-
-                },
-
-                {
-                    "id":5,
-                    "feeling" : 'No Reason',
-                    "isLeaf":false
-
-                }
-            ]*/
 
         } else if (vm.currentStep == 2) {
             // Reason selection
@@ -153,66 +104,36 @@ myApp.controller('HomeController', function($scope, $http, $sce, $localStorage, 
             //$scope.reasonSelections = []
             $scope.reasonPersons = []
 
+            var link = ""
+            if($localStorage.fileType == "Resume") {
+                vm.selectedFileType = $localStorage.fileType;
+             //   link = 'http://localhost:8080/files/resume/59c17b1c77c8e33c1e766dc7'
+            } else {
+                vm.selectedFileType = $localStorage.fileType;
+              //  link = 'http://localhost:8080/files/jd/59c00c6930528df66672e73f'
+            }
 
-            $http({
+           /* $http({
              method: 'GET',
             // url: '/nodes/children/'+vm.user.whyOptionSelection
-                url: 'http://54.210.170.189:9000/nodes/children/'+vm.user.whyOptionSelection
+                url: link
              }).then(function successCallback(response) {
              // this callback will be called asynchronously
              // when the response is available
-                $scope.reasonSelections = response.data
+                //vm.documentTags = response.data;
+                var str = JSON.stringify(response.data, undefined, 4);
+                vm.documentTags = str;
              }, function errorCallback(response) {
              // called asynchronously if an error occurs
              // or server returns response with an error status.
-             });
+             });*/
 
-
-
-            /*
-            $scope.reasonSelections = [
-                {
-                    "id":6,
-                    "feeling" : 'Spouse',
-                    "isLeaf":false
-
-                },
-
-                {
-                    "id":7,
-                    "feeling" : 'Sibling',
-                    "isLeaf":false
-
-                },
-
-                {
-                    "id":8,
-                    "feeling" : 'Manager/Colleague',
-                    "isLeaf":false
-
-                }
-            ]
-
-
-
-            for(var item in $scope.selection) {
-                if($scope.selection[item] == 'Relation Ship') {
-                    $scope.reasonSelections.push('Spouse');
-                    $scope.reasonSelections.push('Sibling');
-                    $scope.reasonSelections.push('Manager/Colleague');
-                } else if ($scope.selection[item] == 'Angry'){
-                    $scope.reasonSelections.push('Offical');
-                    $scope.reasonSelections.push('Wife');
-                    $scope.reasonSelections.push('Family');
-                }
-            }*/
         }
-
 
         vm.currentStep = newStep;
     }
 
-    vm.getStepTemplate = function(){
+    vm.getStepTemplate = function() {
         for (var i = 0; i < vm.steps.length; i++) {
             if (vm.currentStep == vm.steps[i].step) {
                 return vm.steps[i].template;
@@ -220,15 +141,113 @@ myApp.controller('HomeController', function($scope, $http, $sce, $localStorage, 
         }
     }
 
+    $scope.onCategoryChange = function () {
+        $localStorage.fileType= $scope.selectedFileType;
+    };
+
+    $scope.onSuccess = function(response) {
+        // Do something
+        console.log ("uploaded successfully");
+    };
+
+
+    vm.uploadFile = function(){
+
+        var link = ""
+        if($localStorage.fileType == "Resume") {
+            link = 'http://localhost:8080/files/resume/59c17b1c77c8e33c1e766dc7'
+        } else {
+            link = 'http://localhost:8080/files/jd/59c00c6930528df66672e73f'
+        }
+
+       $http.get(link)
+            .then(function(response) {
+                console.log ("uploaded successfully", response);
+                vm.documentTags = response.data;
+                vm.successMsg="Doc Uploaded successfully";
+            });
+
+
+       /*
+        console.log ("uploaded successfully");
+
+        var file = $scope.myFile;
+        console.log('file is ' );
+        console.dir(file);
+        var uploadUrl = "http://localhost:8080/files/resume";
+        //fileUpload.uploadFileToUrl(file, uploadUrl);
+
+        var fd = new FormData();
+        fd.append('file', file);
+
+        $http.post(uploadUrl, fd, {
+                headers: {'Content-Type': undefined}
+            })
+            .success(function () {
+                console.log ("uploaded successfully");
+            })
+            .error(function () {
+                console.log ("upload failed");
+            });*/
+
+    };
+
+
+
+
+
+       /* var data = $scope.file;
+
+        var fd = new FormData();
+        fd.append('file', data);
+
+        console.log("File data" , file);
+
+        $http.post('http://localhost:8080/files/resume', fd, {
+            headers: {'Content-Type': undefined}
+        }).then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+           // $scope.moods = response.data
+            console.log ("uploaded successfully");
+
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            console.log ("upload failed");
+
+        });*/
+
+
+       // var fd = {'file': file};
+
+
+
+ /*$http.post('http://localhost:8080/files/resume', fd, {
+ headers: {'Content-Type': undefined}
+ }).success(function(){
+                console.log ("uploaded successfully");
+            })
+            .error(function(){
+                console.log ("upload failed");
+            });*/
+
+
     vm.save = function() {
 
        // $scope.progress= []
        // SharedDataService = $scope.progress
 
+        if($localStorage.fileType == "Resume") {
+            link = 'http://localhost:8080/files/resume/59c17b1c77c8e33c1e766dc7/rank'
+        } else {
+            link = 'http://localhost:8080/files/jd/59c00c6930528df66672e73f/rank'
+        }
+
         $http({
             method: 'GET',
             //url: '/content/' + vm.user.reasonSelection
-            url: 'http://54.210.170.189:9000/content/'+vm.user.reasonSelection
+            url: link
         }).then(function successCallback(response) {
             // this callback will be called asynchronously
             // when the response is available
